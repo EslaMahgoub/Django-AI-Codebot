4f2f6db7c524eedd6ad53c49daf737b168080534
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2024-06-29 08:53:49 +0200

    Add prismjs for code area
    Add base, home, navbar and messages
    Update home view to handle the code form

--- src/website/views.py
+++ src/website/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, "home.html", {})
+    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'git', 'go', 'java', 'javascript', 'jsx', 'markup', 'markup-templating', 'matlab', 'mongodb', 'perl', 'php', 'powershell', 'python', 'ruby', 'rust', 'sass', 'sql', 'swift', 'tsx', 'typescript', 'yaml']
+    
+    if request.method == 'POST':
+        code = request.POST.get('code')
+        lang = request.POST.get('lang')
+        
+        if lang == "Select Programming Language":
+            messages.error(request, "Please select a programming language")
+            
+        return render(request, "home.html", {"lang_list": lang_list, "code": code, "lang": lang})
+    return render(request, "home.html", {"lang_list": lang_list})
